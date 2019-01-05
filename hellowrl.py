0b342a98d24cb28155f2578ea2106311a920f955
Philip Molina <Philmeista3@gmail.com>
2019-01-05 16:42:28 -0700

    Initial commit

---
+++ hellowrl.py
@@ -0,0 +1,39 @@
+##print("hello world")
+##x=98
+##y=76
+##b=90
+##t=x*y*b
+##print(t)
+##
+##for i in range(9):
+##    print(t)
+##    print('you are awesome')
+##    print (i)
+##a=input()
+##print(a)
+##while input('You are whiteman?')!='bye':
+##    print('Are you leaving already?')
+##
+
+
+
+
+
+
+
+print('You are alone in the world.',
+      'There are no people to be seen or animals to be found.')
+
+path=input('Choose a direction.')
+if path=='north':
+    print('You arive at an abandoned parking lot',
+          'the asphalt is cracked')
+elif path=='south':
+    print('A train is aproaching at rapid speed toward you',
+          'What do you do?')
+    act=input()
+    if act=='jump':
+        print('You land on your ankle badly.')
+      
+    
+    
